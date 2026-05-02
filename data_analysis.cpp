8e274a88ae9ca088bcfb213f62131dbcc652297f
andruha2007 <andruha141207@gmail.com>
2026-05-02 16:29:08 +0300

    Second day with the laboratory work. Approximately final changings

--- data_analysis.cpp
+++ data_analysis.cpp
@@ -14,19 +14,40 @@
 
 
 // это пока что выглядит плохо
-std::unordered_map<std::string, std::unordered_map<std::string, uint16_t>> doc_word_freq; // слова в каждом документе 
-std::unordered_map<std::string, uint16_t> word_freq; // слова во всех документах
+std::unordered_map<std::string, std::unordered_map<std::string, size_t>> doc_word_freq; // слова в каждом документе 
+std::unordered_map<std::string, size_t> word_freq; // слова во всех документах
 std::set<std::string> unique_words;
 std::unordered_map<std::string, size_t> count_of_words; // количество слов в документе
 std::vector<std::string> file_names;
 std::unordered_map<std::string, std::unordered_set<std::string>> count_of_docs_with_word; // df
+std::unordered_map<std::string, std::unordered_set<std::string>> unique_words_in_docs;
+std::unordered_map<std::string, std::unordered_map<std::string, std::vector<size_t>>> doc_word_pos;
+
+bool valide_word(const std::string& word){
+    return count_of_docs_with_word.find(word) == count_of_docs_with_word.end();
+}
+
+bool valide_file_name(const std::string& file_name){
+    return doc_word_freq.find(file_name) == doc_word_freq.end();
+}
+
+const std::string& normalise(const std::string& word){
+    std::string& normalized_word = const_cast<std::string&>(word);
+
+    std::transform(normalized_word.begin(), normalized_word.end(), normalized_word.begin(),
+        [](unsigned char c){ return static_cast<char>(std::tolower(c)); });
+    normalized_word.erase(std::remove_if(normalized_word.begin(), normalized_word.end(),
+        [](unsigned char c){ return !std::isalpha(c); }), normalized_word.end());
+
+    return const_cast<const std::string&>(normalized_word);
+}
 
 double tf(const std::string& file_name, const std::string& word){
-    return static_cast<double>(doc_word_freq.at(file_name).at(word)) / static_cast<double>(count_of_words.at(file_name));
+    return static_cast<double>(doc_word_freq.at(file_name)[word]) / static_cast<double>(count_of_words.at(file_name));
 }
 
 double idf(const std::string& word){
-    return std::log(static_cast<double>(file_names.size()) / static_cast<double>(count_of_docs_with_word.at(word).size()));
+    return std::log(static_cast<double>(file_names.size()) / static_cast<double>(count_of_docs_with_word[word].size()));
 }
 
 double tf_idf(const std::string& file_name, const std::string& word){
@@ -34,6 +55,7 @@ double tf_idf(const std::string& file_name, const std::string& word){
 }
 
 void fill_statistics(const std::string& file_name){
+
     std::ifstream file(file_name);
 
     if (!file.is_open()){
@@ -64,53 +86,138 @@ void fill_statistics(const std::string& file_name){
         std::istream_iterator<std::string>()
     };
 
-    std::unordered_map<std::string, uint16_t> word_freq_in_doc;
-    std::set<std::string> unique_words_in_doc;
+    std::unordered_map<std::string, size_t> word_freq_in_doc;
+    std::unordered_set<std::string> unique_words_in_doc;
 
-    std::for_each(words.begin(), words.end(), [&word_freq_in_doc, &unique_words_in_doc, file_name](const std::string& word){
+    size_t pos = 1;
+
+    std::for_each(words.begin(), words.end(), [&word_freq_in_doc, &unique_words_in_doc, file_name, &pos](const std::string& word){
         ++word_freq_in_doc[word];
         ++word_freq[word];
         unique_words.insert(word);
         unique_words_in_doc.insert(word);
         ++count_of_words[file_name];
+        doc_word_pos[file_name][word].push_back(pos);
+        ++pos;
     });
 
     std::for_each(unique_words_in_doc.begin(), unique_words_in_doc.end(), [file_name](const std::string& word){
         count_of_docs_with_word[word].insert(file_name);
     });
 
-    doc_word_freq[file_name] = word_freq_in_doc; 
+    doc_word_freq[file_name] = word_freq_in_doc;
 
-}
+    unique_words_in_docs[file_name] = unique_words_in_doc;
 
-
-void word_in_doc(const std::string& word, const std::string& file_name){
-    if (doc_word_freq.find(file_name) == doc_word_freq.end()){ // сделать общий валидатор входных аргументов
-        std::cout << "WARNING: [DOCUMENT]: Document \"" << file_name << "\" was not found\n";
-        return;
-    }
-
-    if (doc_word_freq.at(file_name).find(word) == doc_word_freq.at(file_name).end()){ //  нормализовать входное слово так же, как слова из файла
-        std::cout << "WARNING: [WORD]: Word \"" << word << "\" was not found in document \"" << file_name << "\"\n";
-        return;
-    }
-
-    std::cout << "Word: " << word << "\n" << "Document: " << file_name << "\n" 
-            << "Count: " << doc_word_freq.at(file_name).at(word) << "\n" << "TF: " << tf(file_name, word) 
-            << "\n" << "TF-IDF: " << tf_idf(file_name, word) << "\n"; 
 }
 
 void word(const std::string& word){
-    if (count_of_docs_with_word.find(word) == count_of_docs_with_word.end()){
-        std::cout << "WARNING: [WORD]: Word \"" << word << "\" was not found\n";
-        return;
+    const std::string normalised_word = normalise(word);
+    
+    if (valide_word(normalised_word)){
+        throw std::runtime_error("ERROR: [WORD]: Word \"" + word + "\" was not found\n");
     }
 
-    std::cout << "Word: " << word << "\n" << "Documents total: " << count_of_docs_with_word.at(word).size() 
-    << "\n" << "IDF: " << idf(word) << "\n";
-    std::copy(count_of_docs_with_word.at(word).begin(), count_of_docs_with_word.at(word).end(), 
-            std::ostream_iterator<std::string>(std::cout, "\n"));
+    std::cout << "Word: " << normalised_word << "\n" << "Documents total: " << count_of_docs_with_word.at(normalised_word).size()
+    << "\n" << "IDF: " << idf(normalised_word) << "\n";
+
+    int index = 1;
+
+    std::for_each(count_of_docs_with_word.at(normalised_word).begin(), count_of_docs_with_word.at(normalised_word).end(),
+        [&normalised_word, &index](const auto& file_name){
+        std::cout << index << ". " << file_name << " (TF = " << tf(file_name, normalised_word) << ")\n";
+        ++index;
+    });   
 }
+
+void word_in_doc(const std::string& word, const std::string& file_name){
+    if (valide_file_name(file_name)){
+        throw std::runtime_error("ERROR: [DOCUMENT]: Document \"" + file_name + "\" was not found\n");
+    }
+
+    const std::string normalised_word = normalise(word);
+
+    if (valide_word(normalised_word)){
+        throw std::runtime_error("ERROR: [WORD]: Word \"" + word + "\" was not found\n");
+    }
+
+    if (doc_word_freq.at(file_name).find(normalised_word) == doc_word_freq.at(file_name).end()){
+        throw std::runtime_error("ERROR: [WORD]: Word \"" + word + "\" was not found in document \"" + file_name + "\"\n");
+    }
+
+    std::cout << "Word: " << normalised_word << "\n" << "Document: " << file_name << "\n"
+            << "Count: " << doc_word_freq.at(file_name).at(normalised_word) << "\n" << "TF: " << tf(file_name, normalised_word)
+            << "\n" << "TF-IDF: " << tf_idf(file_name, normalised_word) << "\n";
+}
+
+void doc(const std::string& file_name){
+    if (valide_file_name(file_name)){
+        throw std::runtime_error("ERROR: [DOCUMENT]: Document \"" + file_name + "\" was not found\n");
+    }
+
+    std::cout << "Document: " << file_name << "\n" 
+            << "Total words: " <<  count_of_words.at(file_name) << "\n" << "Unique words: "
+            << unique_words_in_docs.at(file_name).size() << "\nTop words:\n";
+
+    std::vector<std::pair<std::string, uint16_t>> sorted_words(doc_word_freq.at(file_name).begin(), doc_word_freq.at(file_name).end());
+    std::sort(sorted_words.begin(), sorted_words.end(), [](const auto& a, const auto& b){
+        if (a.second != b.second){
+            return a.second > b.second; 
+        }
+        return a.first < b.first; 
+    });
+    
+    size_t count = std::min(static_cast<size_t>(5), sorted_words.size());
+    if (count == 0){
+        throw std::runtime_error("ERROR [DOCUMENT]: The document \"" + file_name + "\" is empty");
+    }
+    int index = 1;
+
+    std::for_each(sorted_words.begin(), std::next(sorted_words.begin(), count), [file_name, &index](const auto& pair){
+        std::cout << index << ". " << pair.first << " : (TF = " << tf(file_name, pair.first) << ", IDF = " << idf(pair.first) << ", TF-IDF = " << tf_idf(file_name, pair.first)<< ")\n";
+        ++index;
+    });
+}
+
+double sum(std::vector<std::string> v, const std::string& file_name){
+    double sum = 0;
+    std::for_each(v.begin(), v.end(), [file_name, &sum](std::string& word){sum += tf_idf(file_name, word);});
+    return sum;
+}
+
+void query(int argc, char** argv){
+    std::vector<std::string> phrase(argv + 2, argv + argc);
+    std::vector<std::string> normalised_phrase;
+    std::transform(phrase.begin(), phrase.end(), phrase.begin(), 
+            [normalised_phrase](const std::string& word){return const_cast<std::string&>(normalise(word));});
+
+    std::cout << "Query: ";
+    std::for_each(phrase.begin(), phrase.end(),
+        [](const std::string& word){std::cout << word  << " ";});
+
+    std::cout << "\nResults:\n";
+
+    int index = 1;
+
+    std::for_each(file_names.begin(), file_names.end(),
+        [phrase, &index](const std::string& file_name){
+            std::cout << index << ". " << file_name  << ": (" << sum(phrase, file_name) << ")\n";
+            ++index;
+        });
+
+}
+
+// void query_all(int argc, char** argv){
+
+//     std::vector<std::string> phrase(argv + 2, argv + argc);
+//     std::vector<std::string> normalised_phrase;
+//     std::transform(phrase.begin(), phrase.end(), phrase.begin(), 
+//             [normalised_phrase](const std::string& word){return const_cast<std::string&>(normalise(word));});
+
+//     size_t count_of_phrase = 0;
+//     size_t pos = 0;
+//     std::vector<std::string> docs_with_phrase;
+// }
 
 int main(int argc, char** argv){
     if (argc < 2) throw std::runtime_error("ERRROR [INPUT]: Not enough arguments");
@@ -135,10 +242,14 @@ int main(int argc, char** argv){
 
     } else if (std::string(argv[1]) == "DOC"){
         if (argc < 3) throw std::runtime_error("ERRROR [INPUT]: Not enough arguments"); 
+            doc(std::string(argv[2]));
         
     } else if (std::string(argv[1]) == "QUERY"){
         if (argc < 3) throw std::runtime_error("ERRROR [INPUT]: Not enough arguments"); 
-        
+        query(argc, argv);
+    // } else if (std::string(argv[1]) == "QUERY_ALL"){
+    //     if (argc < 3) throw std::runtime_error("ERRROR [INPUT]: Not enough arguments"); 
+    //     query_all(argc, argv);
     } else {
         throw std::runtime_error("ERRROR [INPUT]: Unknown command");
     }
